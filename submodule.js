8d15e65bb019bb823afd432d97bf2b0f5ed2bef6
Stanislav Sakun <stanislav.sakun@ukr.net>
2023-08-09 15:03:03 +0300

    Farewell

--- submodule.js
+++ submodule.js
@@ -1,3 +1,7 @@
-export default function greeting(name) {
+export function greeting(name) {
   console.log(`Hello ${name}`);
+}
+
+export function farewell(name) {
+  console.log(`Godbye ${name}`);
 }
